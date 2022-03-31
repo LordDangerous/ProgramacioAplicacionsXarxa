c0890227294cba7b492dd9658b78ea56fa860af5
Marc <mamrayo10@gmail.com>
2022-03-31 10:24:48 +0200

    Fixed reading configuration and added loggers

--- client.c
+++ client.c
@@ -23,11 +23,11 @@ void removeSpaces(char* s) {
         while (*d == ' ') {
             ++d;
         }
-    } while (*s++ = *d++);
+    } while ((*s++ = *d++));
 }
 
 
-void printDebug(char* s) {
+void printDebug(char* message) {
     int hours, minutes, seconds;
     time_t now;
     time(&now);
@@ -36,74 +36,122 @@ void printDebug(char* s) {
     hours = local->tm_hour;
     minutes = local->tm_min;
     seconds = local->tm_sec;
-    printf("%d:%d:%d - DEBUG => %s", hours, minutes, seconds, s);
+    printf("%d:%d:%d - DEBUG => %s", hours, minutes, seconds, message);
 }
 
+
+void printInfo(char* message) {
+    int hours, minutes, seconds;
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+
+    hours = local->tm_hour;
+    minutes = local->tm_min;
+    seconds = local->tm_sec;
+    printf("%d:%d:%d - INFO => %s", hours, minutes, seconds, message);
+}
+
+
+void printError(char* message) {
+    int hours, minutes, seconds;
+    time_t now;
+    time(&now);
+    struct tm *local = localtime(&now);
+
+    hours = local->tm_hour;
+    minutes = local->tm_min;
+    seconds = local->tm_sec;
+    printf("%d:%d:%d - ERROR => %s", hours, minutes, seconds, message);
+}
 
 
 void readFile() {
     struct Client client;
     FILE* fp;
     char* p;
+    char* value;
     char line[2048];
     int lineNumber = 0;
     char delimiter[2] = "=";
     fp = fopen(clientFile, "r");
     if (ferror(fp)) {
-        printf("File couldn't be opened");
+        printError("No es pot obrir l'arxiu de configuració");
         exit(1);
     }
     while (fgets(line, sizeof(line), fp) != NULL) {
-        printDebug(line);
         p = strtok(line, delimiter);
-
+        value = strtok(NULL, delimiter);
+        removeSpaces(value);
         
         if (lineNumber == 0){
             if (strcmp(p, "Id ") == 0){
-                p = strtok(NULL, delimiter);
-                removeSpaces(p);
-                strcpy(client.id_client, p);
-                printf(client.id_client);
+                strcpy(client.id_client, value);
+                char* message = malloc(sizeof(char)*100);
+                sprintf(message, "Assignat id_client: %s", client.id_client);
+                printInfo(message);
+                free(message);
             }  
+            else {
+                printInfo("No es pot obtenir l'identificador del client de l'arxiu de configuració");
+                exit(1);
+            }
             
         }
         if (lineNumber == 1){
             if (strcmp(p, "Elements ") == 0){
-                p = strtok(NULL, delimiter);
-                removeSpaces(p);
-                strcpy(client.elements, p);
-                printf(client.elements);
+                strcpy(client.elements, value);
+                char* message = malloc(sizeof(char)*100);
+                sprintf(message, "Assignats elements: %s", client.elements);
+                printInfo(message);
+                free(message);
+            }
+            else {
+                printInfo("No es poden obtenir els elements del client de l'arxiu de configuració");
+                exit(1);
             }
         }
         if (lineNumber == 2){
             if (strcmp(p, "Local-TCP ") == 0){
-                p = strtok(NULL, delimiter);
-                removeSpaces(p);
-                client.tcp_port = atoi(p);
-                printf("%d\n", client.tcp_port);
+                client.tcp_port = atoi(value);
+                char* tcp_port = malloc(sizeof(char)*10);
+                sprintf(tcp_port, "Assignat port tcp: %d\n", client.tcp_port);
+                printInfo(tcp_port);
+                free(tcp_port);
+            }
+            else {
+                printInfo("No es pot obtenir el port tcp del client de l'arxiu de configuració");
+                exit(1);
             }
         }
         if (lineNumber == 3){
             if (strcmp(p, "Server ") == 0){
-                p = strtok(NULL, delimiter);
-                removeSpaces(p);
-                strcpy(client.server, p);
-                printf(client.server);
+                strcpy(client.server, value);
+                char* message = malloc(sizeof(char)*100);
+                sprintf(message, "Assignada IP servidor: %s", client.id_client);
+                printInfo(message);
+                free(message);
+            }
+            else {
+                printInfo("No es pot obtenir la IP servidor de l'arxiu de configuració");
+                exit(1);
             }
         }
         if (lineNumber == 4){
             if (strcmp(p, "Server-UDP ") == 0){
-                p = strtok(NULL, delimiter);
-                removeSpaces(p);
-                client.server_udp = atoi(p);
-                printf("%d\n", client.server_udp);
+                client.server_udp = atoi(value);
+                char* server_udp = malloc(sizeof(char)*10);
+                sprintf(server_udp, "Assignat port udp del servidor: %d\n", client.server_udp);
+                printInfo(server_udp);
+                free(server_udp);
+            }
+            else {
+                printInfo("No es pot obtenir el port udp del servidor de l'arxiu de configuració");
+                exit(1);
             }
         }
         lineNumber++;
     }
-    
-
-
     fclose(fp);
 }
 
